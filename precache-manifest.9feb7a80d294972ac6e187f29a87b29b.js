0e0b36eab714fcbb5dbb2690aca9fbbaa6fa1d8f
Jacky (Jie) Liu <jackyjieliu@gmail.com>
2020-05-02 15:53:31 +0800

    Updates

---
+++ precache-manifest.9feb7a80d294972ac6e187f29a87b29b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a3394bec661a9d6f7ca5da00872515",
+    "url": "/youtube-video-search/index.html"
+  },
+  {
+    "revision": "f929b236fa277ade60f7",
+    "url": "/youtube-video-search/static/css/main.d036ee29.chunk.css"
+  },
+  {
+    "revision": "c55b67355910d7bcd8d9",
+    "url": "/youtube-video-search/static/js/2.afd2af96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/youtube-video-search/static/js/2.afd2af96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f929b236fa277ade60f7",
+    "url": "/youtube-video-search/static/js/main.1920d9ef.chunk.js"
+  },
+  {
+    "revision": "7b4df7048d140e521b13",
+    "url": "/youtube-video-search/static/js/runtime-main.a9ed13a4.js"
+  }
+]);
